4fddc13903411e37bab4f22ec83922434b4c6c7e
Agung Miftakhul Huda <43521451+Agungmifta@users.noreply.github.com>
2018-09-30 19:44:06 +0700

    proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,30 @@
-<?php
+<?php 
+session_start();
+$tmpl1=$_SESSION['arr1'];
+$tmpl2=$_SESSION['arr2'];
+$tmpl3=$_SESSION['arr3'];
+ ?>
+<table>
+	<tr>
+		<td>USER</td>
+		<td><?php foreach ($tmpl1 as $nilai1): 
+			 echo $nilai1. "<br>"; ?>
+			 <?php endforeach ?></td>
+		</td>
+	</tr>
+	<tr>
+		<td>FILM:</td>
+		<td><?php foreach ($tmpl2 as $nilai2):
+			 echo $nilai2."<br>"; ?>
+		<?php endforeach ?></td>
+	</tr>
 
-
-?>
+	<tr>
+		<td>TEMPAT WISATA:</td>
+		<td><?php foreach ($tmpl3 as $nilai3):
+			 echo $nilai3."<br>"; ?>
+			 <?php endforeach ?></td>
+		</td>
+	</tr>
+	
+</table>
